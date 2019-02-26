8c9e79ccfea95399075bb482b5af07a91ec79659
dceoy <dnarsil+github@gmail.com>
2019-02-26 21:49:09 +0900

    Update ewma model

--- fract/model/ewma.py
+++ fract/model/ewma.py
@@ -2,7 +2,6 @@
 
 import logging
 import numpy as np
-from scipy import stats
 from .sieve import LRFeatureSieve
 
 
@@ -10,7 +9,7 @@ class Ewma(object):
     def __init__(self, config_dict):
         self.__logger = logging.getLogger(__name__)
         self.__alpha = config_dict['model']['ewma']['alpha']
-        self.__ci_level = config_dict['model']['ewma'].get('ci_level')
+        self.__sigma_band = config_dict['model']['ewma']['sigma_band']
         self.__lrfs = LRFeatureSieve(
             type=config_dict['feature']['type'],
             weight_decay=config_dict['model']['ewma']['alpha']
@@ -19,9 +18,9 @@ class Ewma(object):
     def detect_signal(self, history_dict, pos=None):
         best_f = self.__lrfs.extract_best_feature(history_dict=history_dict)
         sig_dict = self._ewm_stats(series=best_f['series'])
-        if sig_dict['ewmci'][0] > 0:
+        if sig_dict['ewmbb'][0] > 0:
             sig_act = 'buy'
-        elif sig_dict['ewmci'][1] < 0:
+        elif sig_dict['ewmbb'][1] < 0:
             sig_act = 'sell'
         elif pos and (
                 (pos['side'] == 'buy' and sig_dict['ewma'] < 0) or
@@ -34,23 +33,22 @@ class Ewma(object):
                 self.__lrfs.code, best_f['granularity_str'],
                 '{:.1g}'.format(sig_dict['ewma']),
                 np.array2string(
-                    sig_dict['ewmci'],
+                    sig_dict['ewmbb'],
                     formatter={'float_kind': lambda f: '{:.1g}'.format(f)}
                 )
             )
         )
         return {
             'sig_act': sig_act, 'sig_log_str': sig_log_str,
-            'sig_ewma': sig_dict['ewma'], 'sig_ewmcil': sig_dict['ewmci'][0],
-            'sig_ewmciu': sig_dict['ewmci'][1]
+            'sig_ewma': sig_dict['ewma'], 'sig_ewmbbl': sig_dict['ewmbb'][0],
+            'sig_ewmbbu': sig_dict['ewmbb'][1]
         }
 
     def _ewm_stats(self, series):
         ewm = series.ewm(alpha=self.__alpha)
         ewma = ewm.mean().iloc[-1]
         self.__logger.debug('ewma: {}'.format(ewma))
-        n_ewm = len(series)
-        ewmci = np.asarray(
-            stats.t.interval(alpha=self.__ci_level, df=(n_ewm - 1))
-        ) * ewm.std().iloc[-1] / np.sqrt(n_ewm) + ewma
-        return {'ewma': ewma, 'ewmci': ewmci}
+        ewm_bollinger_band = (
+            np.array([-1, 1]) * ewm.std().iloc[-1] * self.__sigma_band
+        ) + ewma
+        return {'ewma': ewma, 'ewmbb': ewm_bollinger_band}
