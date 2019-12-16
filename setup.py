6d7542405e8188ff71b7e5715e380c3d49faf27f
dceoy <dnarsil+github@gmail.com>
2019-12-17 03:59:33 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,13 @@ with open('README.md', 'r') as f:
 setup(
     name='fract',
     version=__version__,
-    description='Automated Trader using Oanda V20 REST API',
-    packages=find_packages(),
     author='Daichi Narushima',
     author_email='dnarsil+github@gmail.com',
+    description='Automated Trader using Oanda V20 REST API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/dceoy/fract',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'docopt', 'numpy', 'pandas', 'pyyaml', 'redis', 'scikit-learn',
@@ -23,18 +25,16 @@ setup(
     dependency_links=[
         'git+https://github.com/dceoy/oanda-cli.git#egg=oanda-cli'
     ],
-    entry_points={
-        'console_scripts': ['fract=fract.cli.main:main'],
-    },
+    entry_points={'console_scripts': ['fract=fract.cli.main:main']},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Financial and Insurance Industry',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
         'Programming Language :: Python :: 3',
         'Topic :: Office/Business :: Financial :: Investment'
     ],
-    python_requires='>=3.6',
-    long_description=long_description
+    python_requires='>=3.6'
 )
