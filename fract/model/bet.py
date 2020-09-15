a0703f9a3d7dae2caad4714f64e833224a4cd0d8
dceoy <dnarsil+github@gmail.com>
2020-09-15 19:11:32 +0900

    Update fract/model/bet.py

--- fract/model/bet.py
+++ fract/model/bet.py
@@ -23,47 +23,42 @@ class BettingSystem(object):
             raise ValueError('invalid strategy name')
 
     def calculate_size_by_pl(self, unit_size, inst_pl_txns, init_size=None):
-        pl_list = [float(t['pl']) for t in inst_pl_txns if float(t['pl']) != 0]
         size_list = [
-            abs(int(float(t['units']))) for t in inst_pl_txns
-            if float(t['units']) != 0
+            float(t['units']) for t in inst_pl_txns if float(t['units']) != 0
         ]
-        if not (pl_list and size_list):
-            return init_size or unit_size
+        last_size = abs(int(size_list[-1] if size_list else 0))
+        self.__logger.debug(f'last_size:\t{last_size}')
+        pl = pd.Series([
+            t['pl'] for t in inst_pl_txns
+        ]).astype(float).pipe(lambda a: a[a!= 0])
+        if pl.size == 0:
+            return last_size or init_size or unit_size
         else:
-            last_size = size_list[-1]
-            self.__logger.debug(f'last_size:\t{last_size}')
-            if abs(pl_list[-1]):
-                pl = pd.Series(pl_list)
-                last_won = (
-                    None if (
-                        pl.iloc[-1] > 0 and any(pl.le(0)) and
-                        pl[pl.index >= pl[pl.le(0)].index.max()].sum() < 0
-                    ) else (pl.iloc[-1] > 0)
-                )
-                self.__logger.debug(f'last_won:\t{last_won}')
-                return self._calculate_size(
-                    unit_size=unit_size, init_size=init_size,
-                    last_size=last_size, last_won=last_won,
-                    all_time_high=(pl.cumsum().idxmax() == pl.index[-1])
-                )
-            else:
-                return last_size
+            won_last = (
+                None if (pl.size > 1 and pl.iloc[-1] > 0 and pl[-2:].sum() < 0)
+                else (pl.iloc[-1] > 0)
+            )
+            self.__logger.debug(f'won_last:\t{won_last}')
+            return self._calculate_size(
+                unit_size=unit_size, init_size=init_size,
+                last_size=last_size, won_last=won_last,
+                all_time_high=(pl.cumsum().idxmax() == pl.index[-1])
+            )
 
     def _calculate_size(self, unit_size, init_size=None, last_size=None,
-                        last_won=None, all_time_high=False):
-        if last_won is None:
+                        won_last=None, all_time_high=False):
+        if won_last is None:
             return last_size or init_size or unit_size
         elif self.strategy == 'Martingale':
-            return (unit_size if last_won else last_size * 2)
+            return (unit_size if won_last else last_size * 2)
         elif self.strategy == 'Paroli':
-            return (last_size * 2 if last_won else unit_size)
+            return (last_size * 2 if won_last else unit_size)
         elif self.strategy == "d'Alembert":
-            return (unit_size if last_won else last_size + unit_size)
+            return (unit_size if won_last else last_size + unit_size)
         elif self.strategy == "Reverse d'Alembert":
-            return (last_size + unit_size if last_won else unit_size)
+            return (last_size + unit_size if won_last else unit_size)
         elif self.strategy == 'Pyramid':
-            if not last_won:
+            if not won_last:
                 return (last_size + unit_size)
             elif last_size < unit_size:
                 return last_size
@@ -73,7 +68,7 @@ class BettingSystem(object):
             self.__logger.debug(f'all_time_high:\t{all_time_high}')
             if all_time_high:
                 return init_size or unit_size
-            elif last_won:
+            elif won_last:
                 return (last_size + unit_size)
             else:
                 return last_size
